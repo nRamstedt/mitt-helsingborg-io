b29f7d218153dc4e3d0bf01963152f1efaf46539
Ehsan Zilaei <ehsan.zilaei@helsingborg.se>
2019-09-25 09:31:05 +0200

    Changed endpoint form to forms.

--- src/components/forms/dal.js
+++ src/components/forms/dal.js
@@ -1,35 +1,5 @@
 const axios = require('axios');
-const mysql = require('mysql');
 const logger = require('../../utils/logger');
-
-exports.saveForm = async inputData => new Promise(((resolve, reject) => {
-  try {
-    const db = mysql.createConnection({
-      host: process.env.DBHOST,
-      port: process.env.DBPORT,
-      user: process.env.DBUSER,
-      password: process.env.DBPASSWORD,
-      database: process.env.DBNAME,
-    });
-
-    db.connect((err) => {
-      if (err) {
-        reject(err);
-      }
-    });
-
-    db.query('INSERT INTO forms SET ?', inputData, (error, results, fields) => {
-      if (error) {
-        reject(error);
-      }
-      resolve(results.insertId);
-    });
-
-    db.end();
-  } catch (error) {
-    reject(error);
-  }
-}));
 
 exports.getAllForms = async () => {
   const endpoint = `${process.env.FORM_SERVICE_URL}/forms/`;
--- src/components/routes.js
+++ src/components/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const authentication = require('./auth/api');
 const user = require('./user/api');
-const form = require('./form/api');
+const forms = require('./forms/api');
 const notification = require('./notification/api');
 const payment = require('./payment/api');
 const chatbot = require('./chat/api');
@@ -12,7 +12,7 @@ const router = express.Router();
 // Register route to api-layer.
 router.use('/auth', authentication);
 router.use('/user', user);
-router.use('/form', form);
+router.use('/forms', forms);
 router.use('/notification', notification);
 router.use('/payment', payment);
 router.use('/chatbot', chatbot);
