b0f440c4b74f6acaf16a2e9bceed7b39c97e2348
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2019-09-25 09:10:55 +0200

    User routes function. Create json api response
    Respective service handles it’s own json api serialization.

--- src/components/routes.js
+++ src/components/routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 
+// Require underlying service definitions (jsonapi ahndled by respective service)
 const authentication = require('./auth/api');
 const user = require('./user/api');
 const form = require('./form/api');
@@ -7,14 +8,30 @@ const notification = require('./notification/api');
 const payment = require('./payment/api');
 const chatbot = require('./chat/api');
 
-const router = express.Router();
+const routes = () => {
+    
+  const router = express.Router();
 
-// Register route to api-layer.
-router.use('/auth', authentication);
-router.use('/user', user);
-router.use('/form', form);
-router.use('/notification', notification);
-router.use('/payment', payment);
-router.use('/chatbot', chatbot);
+  router.get('/', async (req, res) => res.json({
+    jsonapi: {
+      version: '1.0',
+      meta: {
+        service: 'mitt-helsingborg-io',
+        owner: 'Helsingborg Stad',
+        description: 'Main touchpoint for mitt helsingborg app, webpage and assistants.',
+      },
+    },
+  }));
 
-module.exports = router;
+  // Register route to api-layer.
+  router.use('/auth', authentication);
+  router.use('/user', user);
+  router.use('/form', form);
+  router.use('/notification', notification);
+  router.use('/payment', payment);
+  router.use('/chatbot', chatbot);
+
+  return router;
+};
+
+module.exports = routes;
