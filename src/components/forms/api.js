96f2c2d381f2a6d780d12e3d405860ee3be676b7
Teddy Andersson <teddyandersson@Teddys-MBP.brabit.se>
2019-10-03 22:43:59 +0200

    Refactoring forms endpoint
    The forms endpoint is now up to date with the new structure that has been set in helsingborg-io-boilerplate.

--- src/components/forms/api.js
+++ src/components/forms/api.js
@@ -1,49 +1,8 @@
 const router = require('express').Router();
 const dal = require('./dal');
-const logger = require('../../utils/logger');
 
-router.get('/', async (req, res) => {
-  try {
-    const reqForms = await dal.getAllForms();
-
-    return res.json(reqForms);
-  } catch (error) {
-    const errorMsg = error.message;
-    logger.error(errorMsg);
-
-    return res.json(errorMsg);
-  }
-});
-
-router.get('/:formId', async (req, res) => {
-  const { formId } = req.params;
-
-  try {
-    const reqForm = await dal.getFormTemplate(formId);
-
-    return res.json(reqForm);
-  } catch (error) {
-    const errorMsg = error.message;
-    logger.error(errorMsg);
-
-    return res.json(errorMsg);
-  }
-});
-
-router.get('/:formId/questions', async (req, res) => {
-  const { formId } = req.params;
-  logger.debug('form api');
-
-  try {
-    const reqForm = await dal.getFormQuestions(formId);
-
-    return res.json(reqForm);
-  } catch (error) {
-    const errorMsg = error.message;
-    logger.error(errorMsg);
-
-    return res.json(errorMsg);
-  }
-});
+router.get('/', async (req, res) => dal.read.forms(req, res));
+router.get('/:formId', async (req, res) => dal.read.form(req, res));
+router.get('/:formId/questions', async (req, res) => dal.read.questions(req, res));
 
 module.exports = router;
