63fd2cfedc4a40a18ce38c48f7d191c74cd690e1
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2020-01-16 11:47:45 +0100

    Add hello world test

--- src/components/routes.test.js
+++ src/components/routes.test.js
@@ -1,26 +1,13 @@
 /* eslint-disable no-unused-expressions */
 require('dotenv').config({ path: './.env.test' });
+
 const chai = require('chai');
-const chaiHttp = require('chai-http');
-const server = require('../server');
 
 const should = chai.should();
-chai.use(chaiHttp);
 
-describe('Routes', () => {
-  after(async () => {
-    server.close();
+describe('Hello World', () => {
+  it('True should be true', async () => {
+    const truthfulVariable = true;
+    truthfulVariable.should.equal(true);
   });
-
-  it('should return json basic response on GET', async () =>
-    chai
-      .request(server)
-      .get('/api/v1')
-      .send()
-      .then(res => {
-        res.should.have.status(200);
-        res.should.be.json;
-        should.exist(res.body);
-        should.exist(res.body.jsonapi);
-      }));
 });
